edf22dd7a5acd1541e0c1f01dd7edbfe1c7ba106
Winston Galas <BrianisWinston@yahoo.com>
2018-09-17 19:59:21 -0700

    Solve problems

---
+++ Hackerrank/10_Days_of_Javascript/Day_1_Let_and_Const.js
@@ -0,0 +1,10 @@
+function main() {
+  // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+  let num = readLine();
+  const PI = Math.PI;
+  console.log(PI * (num**2));
+  console.log(2 * PI * num);
+  // Print the area of the circle:
+
+  // Print the perimeter of the circle:
+end
