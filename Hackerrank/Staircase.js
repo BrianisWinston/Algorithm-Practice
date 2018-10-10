86ea85c1b6d41dd329d3a4a553a66e19525435ce
Winston Galas <BrianisWinston@yahoo.com>
2018-10-09 23:59:48 -0700

    Start stairs question

---
+++ Hackerrank/Staircase.js
@@ -0,0 +1,17 @@
+function staircase(n) {
+    let stairs = [];
+    for(let i = n;i > 0;i--) {
+        let hashes = n - i;
+        let spaces = n - hashes;
+        for(let i2=spaces;i2 >= 0;i2--) {
+            stairs.push(' ');
+        }
+        for(let i3=hashes;i3 >= 0;i3--) {
+            stairs.push('#')
+        }
+        if (stairs.includes('#')) {
+            console.log(stairs.join(''));
+        }
+        stairs = [];
+    }
+}
