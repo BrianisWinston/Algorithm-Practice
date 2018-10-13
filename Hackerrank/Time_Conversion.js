3e67992d7865d1d93249f32ea9c9c1e22626d83b
Winston Galas <BrianisWinston@yahoo.com>
2018-10-13 12:19:27 -0700

    Solve question

--- Hackerrank/Time_Conversion.js
+++ Hackerrank/Time_Conversion.js
@@ -1,12 +1,16 @@
 function timeConversion(s) {
     let answer = s.substr(0, 8);
-    if (s.substr(s.length - 2,2) === 'PM') {
+    if (s.substr(s.length - 2,2) === 'PM' && s.substr(0,2) !== '12') {
         let firstNums = answer.substr(0, 2);
         firstNums = parseInt(firstNums) + 12
+        if (firstNums === 24) {
+            firstNums = "00"
+        }
         answer = firstNums + answer.substr(2, 6);
-    }
-    if (answer.substr(0, 2) === '24') {
-        answer = "00" + answer.substr(2, answer.length - 3);
+    } else if (s.substr(s.length - 2,2) === 'AM' && answer.substr(0, 2) === '12') {
+        answer = "00" + answer.substr(2, answer.length - 2);
     }
     return answer;
 }
+
+console.log('12:40:22AM === 00:40:22', timeConversion('12:45:54PM'))
