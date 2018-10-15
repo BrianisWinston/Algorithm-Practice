4b0c31b371123a5dae20be51946bf678837a0cf6
Winston Galas <BrianisWinston@yahoo.com>
2018-10-15 16:46:24 -0700

    Solve question

--- Hackerrank/Kangaroo.js
+++ Hackerrank/Kangaroo.js
@@ -1,7 +1,10 @@
 function kangaroo(x1, v1, x2, v2) {
-    let first = [x1, x2].sort()[1];
-    let second = [x1, x2].sort()[0];
-    while (first >= second) {
+    let first = x1;
+    let second = x2
+    if (v1 < v2 || v1 === v2) {
+        return 'NO';
+    }
+    while (first <= second) {
         first += v1;
         second += v2;
         if (first === second) {
