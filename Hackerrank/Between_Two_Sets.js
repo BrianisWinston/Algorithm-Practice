a6879811ad7c3ffb503e3cb737b4e540ed26aa3c
Winston Galas <BrianisWinston@yahoo.com>
2018-10-17 20:28:49 -0700

    Added more logic

--- Hackerrank/Between_Two_Sets.js
+++ Hackerrank/Between_Two_Sets.js
@@ -2,19 +2,23 @@ function getTotalX(a, b) {
     let sortedNums = b.sort((a, b) => a - b)
     let nums = factorials(a[a.length-1], sortedNums[0]);
     let newNums = [];
+    let answer = [];
+    let facts = true;
+
     if (sortedNums.length > 1) {
         for (let i = 0; i < nums.length; i++) {
             for (let i2 = 1; i2 < sortedNums.length; i2++) {
-                if (b[i2] % sortedNums[i] === 0 && !newNums.includes(sortedNums[i]))
-                    newNums.push(sortedNums[i])
+                if (sortedNums[i2] % nums[i] === 0 && !newNums.includes(nums[i]))
+                    newNums.push(nums[i])
             }
         }
+    } else {
+      newNums = nums;
     }
-    let answer = [];
-    let facts = true;
+
     for (let i = 0; i < newNums.length; i++) {
         for (let i2 = 0; i2 < a.length; i2++) {
-            if (nums[i] % a[i2] !== 0) {
+            if (newNums[i] % a[i2] !== 0) {
                 facts = false;
             }
         }
@@ -23,7 +27,9 @@ function getTotalX(a, b) {
         }
         facts = true;
     }
-    return answer;
+    console.log(newNums);
+
+    return answer.length;
 }
 
 function factorials(begin, end) {
@@ -37,6 +43,8 @@ function factorials(begin, end) {
 }
 
 
-let a = [2];
-let b = [20, 30, 12];
+// let a = [2];
+// let b = [20, 30, 12];
+let a = [1];
+let b = [100];
 console.log(getTotalX(a, b))
