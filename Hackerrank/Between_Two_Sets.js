53c8b9dfc7efe2962a24814e73457433150b56d3
Winston Galas <BrianisWinston@yahoo.com>
2018-10-17 19:42:11 -0700

    More progress

--- Hackerrank/Between_Two_Sets.js
+++ Hackerrank/Between_Two_Sets.js
@@ -1,8 +1,18 @@
 function getTotalX(a, b) {
-    let nums = factorials(a[a.length-1], b[0]);
+    let sortedNums = b.sort((a, b) => a - b)
+    let nums = factorials(a[a.length-1], sortedNums[0]);
+    let newNums = [];
+    if (sortedNums.length > 1) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let i2 = 1; i2 < sortedNums.length; i2++) {
+                if (b[i2] % sortedNums[i] === 0 && !newNums.includes(sortedNums[i]))
+                    newNums.push(sortedNums[i])
+            }
+        }
+    }
     let answer = [];
     let facts = true;
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < newNums.length; i++) {
         for (let i2 = 0; i2 < a.length; i2++) {
             if (nums[i] % a[i2] !== 0) {
                 facts = false;
@@ -13,15 +23,5 @@ function getTotalX(a, b) {
         }
         facts = true;
     }
-    return answer.length;
-}
-
-function factorials(begin, end) {
-    let facts = [];
-    for (let i = begin; i <= end; i++) {
-        if (end % i === 0) {
-            facts.push(i);
-        }
-    }
-    return facts;
+    return answer;
 }
