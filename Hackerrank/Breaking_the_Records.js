26e65cc7952269597a0565ea111fd570db88d238
Winston Galas <BrianisWinston@yahoo.com>
2018-10-19 21:43:37 -0700

    Answer question

--- Hackerrank/Breaking_the_Records.js
+++ Hackerrank/Breaking_the_Records.js
@@ -1,4 +1,16 @@
 function breakingRecords(scores) {
-
-
+  let min = scores[0];
+  let max = scores[0];
+  let minChanges = 0;
+  let maxChanges = 0;
+  for ( let i = 1 ; i < scores.length ; i++ ) {
+      if (scores[i] < min) {
+          min = scores[i];
+          minChanges++;
+      } else if (scores[i] > max) {
+          max = scores[i];
+          maxChanges++
+      }
+  }
+  return [maxChanges, minChanges];
 }
