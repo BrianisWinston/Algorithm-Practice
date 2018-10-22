b9b926802e13509ca42d628734fc8ab452897af9
Winston Galas <BrianisWinston@yahoo.com>
2018-10-21 22:16:54 -0700

    Solve question

--- Hackerrank/Birthday_Chocolate.js
+++ Hackerrank/Birthday_Chocolate.js
@@ -1,7 +1,7 @@
 function birthday(s, d, m) {
     let answer = 0;
     let total = 0;
-    for (let i = m;i < s.length;i++) {
+    for (let i = m;i <= s.length;i++) {
         for(let i2 = i - m;i2 < i; i2++) {
             total += s[i2];
             // console.log(s[i2])
