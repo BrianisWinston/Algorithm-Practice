690a1e195d8c095cc1606d9f51e849c6494ac148
Winston Galas <BrianisWinston@yahoo.com>
2018-10-25 14:06:23 -0700

    Solve question

--- Hackerrank/Migratory_Birds.rb
+++ Hackerrank/Migratory_Birds.rb
@@ -3,5 +3,7 @@ def migratoryBirds(arr)
     arr.each { |el| answer[el] += 1}
     new = answer.sort_by { |k, v| v }
     newnew = new.select { |k, v| v == new[-1][1]}
+    newnew = newnew.sort_by { |k, v| k}
+    print newnew[0][0]
     newnew[0][0]
 end
