0e20fa224d4953912faf153f0e05111ef7c4ff4a
Winston Galas <BrianisWinston@yahoo.com>
2018-10-27 21:38:45 -0700

    Pass most test cases

--- Hackerrank/Day_of_the_Programmer.js
+++ Hackerrank/Day_of_the_Programmer.js
@@ -4,15 +4,16 @@ function dayOfProgrammer(year) {
         if (year % 4 === 0) {
             answer = `12.09.${year}`
         } else {
-            answer = `12.08.${year}`
+            answer = `13.09.${year}`
         }
     }   else if (year === 1918) {
-
+        answer = `30.08.${year}`
     }   else {
-        if (year % 4 === 0) {
+        if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
             answer = `12.09.${year}`
         } else {
-            answer = `12.08.${year}`
+            answer = `13.09.${year}`
         }
     }
+    return answer;
 }
