0b2fda36435ba275c22905ad9e09470eaf612151
Winston Galas <BrianisWinston@yahoo.com>
2018-11-26 21:28:42 -0800

    2nd try

--- Hackerrank/Drawing_Book.js
+++ Hackerrank/Drawing_Book.js
@@ -4,11 +4,7 @@ function pageCount(n, p) {
         answer = Math.floor(p / 2)
     } else {
         let newNum = n - p;
-        if (p % 2 !== 0) {
-            answer = Math.floor(newNum / 2) + 1;
-        } else {
-            answer = Math.floor(newNum / 2);
-        }
+        answer = Math.floor(newNum / 2);
     }
     return answer;
 }
