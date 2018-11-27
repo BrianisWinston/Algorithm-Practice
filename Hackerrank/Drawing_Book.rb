30d7e7e1b41b7f962d8ae4c0a234e3c3a65c2ce4
Winston Galas <BrianisWinston@yahoo.com>
2018-11-26 21:46:22 -0800

    STart question

--- Hackerrank/Drawing_Book.rb
+++ Hackerrank/Drawing_Book.rb
@@ -1,10 +1,10 @@
 def pageCount(n, p)
-    if ( n / 2 >= p )
-        answer = p / 2
-    elsif ()
+    if ( n.to_f / 2 >= p )
+        answer = p.to_f / 2
+    else
         new_num = n - p
-        answer = (new_num / 2)
+        answer = (new_num.to_f / 2)
         answer += 1 if ( n % 2 == 0 && p % 2 != 0 )
     end
-    answer
+    answer.floor
 end
