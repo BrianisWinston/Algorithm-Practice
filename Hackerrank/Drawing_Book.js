7e0e06e66977ed776da16f67eda39d604690b39d
Winston Galas <BrianisWinston@yahoo.com>
2018-11-26 21:32:53 -0800

    Solve question

--- Hackerrank/Drawing_Book.js
+++ Hackerrank/Drawing_Book.js
@@ -1,10 +1,13 @@
 function pageCount(n, p) {
     let answer;
-    if (n / 2 > p) {
+    if (n / 2 >= p) {
         answer = Math.floor(p / 2)
     } else {
         let newNum = n - p;
         answer = Math.floor(newNum / 2);
+        if (n % 2 === 0 && p % 2 !== 0) {
+            answer+= 1
+        }
     }
     return answer;
 }
