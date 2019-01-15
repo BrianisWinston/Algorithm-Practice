96a1ddadf9270d896d3bcdc64b67909af5843e6c
Winston Galas <BrianisWinston@yahoo.com>
2019-01-14 21:52:26 -0800

    Solve subsets

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,5 +1,22 @@
 const subset = (arr) => {
+  // if (arr.length === 0) {
+  //   return [[]];
+  // }
+  //
+  // let slices = subset(arr.slice(1));
+  // console.log(slices);
+  // return slices;
+  if (arr.length === 0) {
+    return [[]];
+  }
+  debugger
+  const first = arr[0];
+  const subs = subset(arr.slice(1));
+  console.log(subs);
 
+  const newSubs = subs.map(sub => [first].concat(sub) );
+
+  return subs.concat(newSubs);
 }
 
 console.log(subset([1, 2, 3]));
