41bcc213ab831d69213120f316a2165d2ffe62aa
Winston Galas <BrianisWinston@yahoo.com>
2019-01-16 18:59:21 -0800

    clean up code

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -6,16 +6,8 @@ const subset = (arr) => {
   let first = arr[0];
   let sliced = subset(arr.slice(1));
 
-  let newSubs = sliced.map( sub => {
-    let wsup = [first];
-    if (sub instanceof Array) {
-      sub.forEach( el => wsup.push(el));
-    } else {
-      wsup.push(el)
-    };
-    return wsup
-  })
+  let newSubs = sliced.map( sub => [first].concat(sub));
   return sliced.concat(newSubs);
 }
 
-console.log(subset([1, 2, 3, 4, 5]));
+console.log(subset([1, 2, 3]));
