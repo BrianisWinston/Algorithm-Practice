0f5872b724c7b8865e2e8e1a3bcd60666a450863
Winston Galas <BrianisWinston@yahoo.com>
2019-01-18 22:11:59 -0800

    First try

--- Common_Concepts/bubbleSort.js
+++ Common_Concepts/bubbleSort.js
@@ -1,5 +1,16 @@
 Array.prototype.bubbleSort = () => {
-  
-}
+  // console.log(arguments);
+  let sorted = false;
+  while (sorted !== true) {
+    sorted = true;
+    for (let i = 0; i < this.length - 2; i++) {
+      if (this[i] > this[i + 1]) {
+        [this[i], this[i + 1]] = [this[i + 1], this[i]];
+      }
+    }
+    sorted;
+  }
+  return this;
+};
 
-console.log([1,6,3,8,6,5,10].bubbleSort()); // [1,3,5,6,6,8.10]
+console.log(Array.prototype.bubbleSort.apply(null,[1,6,3,8,6,5,10])); // [1,3,5,6,6,8.10]
