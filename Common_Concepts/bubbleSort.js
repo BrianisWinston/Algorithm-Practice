0e5b0d8a222e6744e56bc4a68308b355fe83aa23
Winston Galas <BrianisWinston@yahoo.com>
2019-01-18 22:37:13 -0800

    Solve problem

--- Common_Concepts/bubbleSort.js
+++ Common_Concepts/bubbleSort.js
@@ -1,16 +1,36 @@
-Array.prototype.bubbleSort = () => {
-  // console.log(arguments);
+Array.prototype.bubbleSort = function() {
+  // console.log(`yeeee ${this.what}`);
   let sorted = false;
   while (sorted !== true) {
     sorted = true;
     for (let i = 0; i < this.length - 2; i++) {
       if (this[i] > this[i + 1]) {
         [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        sorted = false;
       }
     }
-    sorted;
+    sorted
   }
-  return this;
 };
 
-console.log(Array.prototype.bubbleSort.apply(null,[1,6,3,8,6,5,10])); // [1,3,5,6,6,8.10]
+// console.log(pojo instanceof Object);
+console.log([1,6,3,8,6,5,10].bubbleSort()); // [1,3,5,6,6,8.10]
+
+// Array.prototype.bubbleSort = () => {
+//   console.log(this);
+//   // console.log(`yeeee ${this.what}`);
+//   let sorted = false;
+//   while (sorted !== true) {
+//     sorted = true;
+//     for (let i = 0; i < this.what.length - 2; i++) {
+//       if (this.what[i] > this.what[i + 1]) {
+//         [this.what[i], this.what[i + 1]] = [this.what[i + 1], this.what[i]];
+//         sorted = false;
+//       }
+//     }
+//     sorted;
+//   }
+//   return this.what;
+// };
+
+// console.log(Array.prototype.bubbleSort.call([1,6,3,8,6,5,10]));
