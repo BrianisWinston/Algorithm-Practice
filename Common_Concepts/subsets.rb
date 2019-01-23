bd88e8a72f0ee3dc063d236770437eb17f5b0d7a
Winston Galas <BrianisWinston@yahoo.com>
2019-01-22 23:05:53 -0800

    Solve question

--- Common_Concepts/subsets.rb
+++ Common_Concepts/subsets.rb
@@ -1,5 +1,13 @@
 require 'byebug'
 
+def subsets(arr)
+  return [[]] if arr.empty?
 
+  first = arr[0]
+  sets = subsets(arr[1..-1])
+
+  new_subs = sets.map { |el| [first] + el }
+  sets.concat(new_subs)
+end
 
 p subsets([1, 2, 3])
