edf60b6c01f15345c9d32f60c81710327d423326
Winston Galas <BrianisWinston@yahoo.com>
2019-01-22 23:47:59 -0800

    Solve question

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -2,12 +2,28 @@
 // If a comparator is passed, it should sort according to that callback.
 
 
+Array.prototype.mergeSort = function(comparator) {
+  if (this.length <= 1) return this;
+  if (comparator === undefined) comparator = compA;
 
+  let mid = Math.floor(this.length / 2);
+  let left = this.slice(0, mid).mergeSort(comparator);
+  let right = this.slice(mid).mergeSort(comparator);
 
+  return merge(left, right, comparator);
+}
 
-
-
-
+const merge = (left, right, comp) => {
+  let sorted = [];
+  while (left.length !== 0 && right.length !== 0) {
+    if (comp(left[0], right[0]) <= 0) {
+      sorted.push(right.shift());
+    } else {
+      sorted.push(left.shift());
+    }
+  }
+  return sorted.concat(left, right);
+}
 
 const compA = (a,b) => {
 
@@ -21,5 +37,16 @@ const compA = (a,b) => {
 
 };
 
+const compB = (a, b) => {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 console.log([1,9,2,3,0,5,6,43,24].mergeSort(compA)); // ONE WITH COMPARATOR
+console.log([1,9,2,3,0,5,6,43,24].mergeSort(compB)); // ONE WITH COMPARATOR
 console.log([1,9,2,3,0,5,6,43,24].mergeSort()); // ONE WITHOUT
