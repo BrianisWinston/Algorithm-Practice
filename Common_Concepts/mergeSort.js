10d00975252bef1732b9a0e29fdc070bf038f30c
Winston Galas <BrianisWinston@yahoo.com>
2019-01-22 23:37:39 -0800

    Redo merge sort in js for practice

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -2,33 +2,7 @@
 // If a comparator is passed, it should sort according to that callback.
 
 
-Array.prototype.mergeSort = function(comparator) {
-  if (this.length <= 1) {
-    return this;
-  }
-  let newArr = this.slice()
-  if (comparator === undefined) {
-    comparator = compA;
-  }
-  let mid = Math.floor(newArr.length / 2)
-  let left = newArr.slice(0, mid).mergeSort();
-  let right = newArr.slice(mid).mergeSort();
 
-  return merge(left, right, comparator);
-};
-
-function merge(left, right, comparator) {
-  let sorted = [];
-  // console.log(`asdfsa ${right}`);
-  while (left.length !== 0 && right.length !== 0) {
-    if (comparator(left[0], right[0]) <= 0) {
-      sorted.push(right.shift());
-    } else {
-      sorted.push(left.shift());
-    }
-  }
-  return sorted.concat(left, right);
-}
 
 
 
