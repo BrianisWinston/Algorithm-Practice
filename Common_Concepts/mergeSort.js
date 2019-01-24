8d8551273d91c7dfaad9e4743a88943b271f8e85
Winston Galas <BrianisWinston@yahoo.com>
2019-01-23 23:01:12 -0800

    Start merge sort again

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -2,28 +2,7 @@
 // If a comparator is passed, it should sort according to that callback.
 
 
-Array.prototype.mergeSort = function(comparator) {
-  if (this.length <= 1) return this;
-  if (comparator === undefined) comparator = compA;
 
-  let mid = Math.floor(this.length / 2);
-  let left = this.slice(0, mid).mergeSort(comparator);
-  let right = this.slice(mid).mergeSort(comparator);
-
-  return merge(left, right, comparator);
-}
-
-const merge = (left, right, comp) => {
-  let sorted = [];
-  while (left.length !== 0 && right.length !== 0) {
-    if (comp(left[0], right[0]) <= 0) {
-      sorted.push(right.shift());
-    } else {
-      sorted.push(left.shift());
-    }
-  }
-  return sorted.concat(left, right);
-}
 
 const compA = (a,b) => {
 
