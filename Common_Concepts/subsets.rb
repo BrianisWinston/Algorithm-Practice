cb041cd3cf34881072050935ff4dd3be54adb021
Winston Galas <BrianisWinston@yahoo.com>
2019-01-24 22:41:46 -0800

    Redo subset in ruby for practice

--- Common_Concepts/subsets.rb
+++ Common_Concepts/subsets.rb
@@ -1,9 +1,2 @@
-def subsets(arr)
-  return [[]] if arr.length == 0
-
-  subs = subsets(arr[0..-2])
-  new_subs = subs.map{ |el| el + [arr.last] }
-  subs.concat(new_subs)
-end
 
 p subsets([1, 2, 3])
