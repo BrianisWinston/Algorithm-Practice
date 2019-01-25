42fc415a7c42f519e9de7023756d2ff595c07129
Winston Galas <BrianisWinston@yahoo.com>
2019-01-24 22:26:05 -0800

    Reo merge sort in js for practice

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -2,30 +2,6 @@
 // If a comparator is passed, it should sort according to that callback.
 
 
-Array.prototype.mergeSort = function (cb) {
-  if (this.length === 1) return this;
-  if (cb === undefined) cb = compA;
-
-  let mid = Math.floor(this.length / 2);
-  let left = this.slice(0, mid).mergeSort(cb);
-  let right = this.slice(mid).mergeSort(cb);
-
-  return merge(left, right, cb);
-};
-
-function merge(left, right, cb) {
-  let answer = [];
-  while (left.length !== 0 && right.length !== 0) {
-    if ( cb(left[0], right[0]) <= 0 ) {
-      answer.push(right.shift());
-    } else {
-      answer.push(left.shift());
-    }
-  }
-  answer = answer.concat(left, right);
-  return answer;
-};
-
 const compA = (a,b) => {
 
   if (a > b) {
