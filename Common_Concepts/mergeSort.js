2777c2efbb48322aad89df209ca85e2978828458
Winston Galas <BrianisWinston@yahoo.com>
2019-01-26 19:23:35 -0800

    Redo merge sort in javascript

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -13,36 +13,26 @@ Array.prototype.mergeSort = function (cb) {
 }
 
 const merge = (left, right, cb) => {
-  let answer = [];
-  while (left.length !== 0 && right.length !== 0) {
-    if (cb(left[0], right[0]) <= 0) {
-      answer.push(right.shift());
-    } else {
-      answer.push(left.shift());
-    }
-  }
-  return answer.concat(left, right);
+  
 }
 
-const compA = (a,b) => {
-
+const compA = (a, b) => {
   if (a > b) {
-    return -1;
-  } else if (a < b){
-    return 1;
+    return -1
+  } else if (a === b) {
+    return 0
   } else {
-    return 0;
+    return 1
   }
-
-};
+}
 
 const compB = (a, b) => {
   if (a < b) {
-    return -1;
-  } else if (a > b) {
-    return 1;
+    return -1
+  } else if (a === b) {
+    return 0
   } else {
-    return 0;
+    return 1
   }
 }
 
