9d9875745de2868225f9463695830b3c382adb17
Winston Galas <BrianisWinston@yahoo.com>
2019-01-29 01:38:26 -0800

    Solve subset in js

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,4 +1,11 @@
+const subset = (arr) => {
+  if (arr.length === 0) return [[]];
 
+  let subs = subset(arr.slice(0, arr.length - 1));
+  let newSubs = subs.map( el => el.concat([arr[arr.length - 1]]));
+
+  return subs.concat(newSubs);
+}
 // elegant solution------------
 // const getAllSubsets = theArray => {
 //   theArray.reduce( (subsets, value) => {
