b0863c52d06b21aa6f58da3549782a5eaa0cc9f2
Winston Galas <BrianisWinston@yahoo.com>
2019-02-01 01:08:40 -0800

    Redo merge sort in js

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -1,9 +1,9 @@
-//Write an Array#merge_sort method; it should not modify the original array.
-// If a comparator is passed, it should sort according to that callback.
-
 Array.prototype.mergeSort = function (cb) {
-  if (this.length === 1) return this;
-  if (cb === undefined) cb = compA;
+  if (this.length <= 1) return this;
+
+  if (cb === null) {
+    cb = compA;
+  }
 
   let mid = Math.floor(this.length / 2);
   let left = this.slice(0, mid).mergeSort(cb);
@@ -12,37 +12,25 @@ Array.prototype.mergeSort = function (cb) {
   return merge(left, right, cb);
 }
 
-const merge = (left, right, cb) => {
-  let answer = [];
-  while (left.length !== 0 && right.length !== 0) {
-    if (cb(left[0], right[0]) <= 0) {
-      answer.push(right.shift());
-    } else {
-      answer.push(left.shift());
-    }
-  }
-  return answer.concat(left, right);
-}
-
-const compA = (a, b) => {
-  if (a > b) {
+const compA = (x, y) => {
+  if (x > y) {
     return -1
-  } else if (a === b) {
+  } else if (x === y) {
     return 0
   } else {
     return 1
-  }
-}
+  };
+};
 
-const compB = (a, b) => {
-  if (a < b) {
-    return -1
-  } else if (a === b) {
+const compB = (x, y) => {
+  if (x > y) {
+    return 1
+  } else if (x === y) {
     return 0
   } else {
-    return 1
-  }
-}
+    return - 1
+  };
+};
 
 console.log([1,9,2,3,0,5,6,43,24].mergeSort(compA)); // ONE WITH COMPARATOR
 console.log([1,9,2,3,0,5,6,43,24].mergeSort(compB)); // ONE WITH COMPARATOR
