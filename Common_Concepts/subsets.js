8dc18c3aebe1c446ba753f7af4f27c4e385f6963
Winston Galas <BrianisWinston@yahoo.com>
2019-02-01 00:25:39 -0800

    Solve

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,4 +1,12 @@
+const subset = (arr) => {
+  if (arr.length === 0) return [[]];
 
+  let first = [arr[arr.length - 1]];
+  let subs = subset(arr.slice(0, arr.length - 1));
+  let newSubs = subs.map( el => el.concat(first));
+
+  return subs.concat(newSubs);
+}
 
 // elegant solution------------
 // const getAllSubsets = theArray => {
