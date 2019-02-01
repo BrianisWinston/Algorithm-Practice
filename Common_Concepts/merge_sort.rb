c6a56a57fe3d4b65e0e295bd4ee523f3256d7ab2
Winston Galas <BrianisWinston@yahoo.com>
2019-02-01 01:00:40 -0800

    Solve merge sort in ruby

--- Common_Concepts/merge_sort.rb
+++ Common_Concepts/merge_sort.rb
@@ -1,4 +1,28 @@
+class Array
+  def merge_sort(&prc)
+    return self if self.length <= 1
+    prc ||= Proc.new { |x, y| y <=> x }
+    mid = self.length / 2
+    left = self[0...mid].merge_sort(&prc)
+    right = self[mid..-1].merge_sort(&prc)
 
+    return merge(left, right, &prc)
+  end
+
+  def merge(left, right, &prc)
+    answer = []
+    until (left.empty? || right.empty?)
+      if (prc.call(left[0], right[0]) <= 0)
+        answer << right.shift
+      else
+        answer << left.shift
+      end
+    end
+    answer.concat(left)
+    answer.concat(right)
+    answer
+  end
+end
 
 prc1 = Proc.new { |x, y| x <=> y }
 prc2 = Proc.new { |x, y| y <=> x }
