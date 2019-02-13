ac58971522ca2f0709d57306bb43cf3a9a59ab5a
Winston Galas <BrianisWinston@yahoo.com>
2019-02-13 01:38:53 -0800

    Solve permutations again

--- Common_Concepts/permutation.js
+++ Common_Concepts/permutation.js
@@ -1,17 +1,17 @@
-Array.prototype.permutations = function () {
+Array.prototype.permutations = function() {
   if (this.length <= 1) return [this];
-
+  debugger
   let perms = this.slice(0, this.length - 1).permutations();
   let totalPerms = [];
-  let last = this[this.length - 1];
-  perms.forEach( el => {
-    for (let i = 0; i <= el.length; i++) {
-      let firstHalf = el.slice(0, i);
-      let secondHalf = el.slice(i, el.length);
-      totalPerms.push(firstHalf.concat(last, secondHalf));
+  let last = [this[this.length - 1]];
+  for (let arr of perms) {
+    for (let i = 0; i <= arr.length; i++) {
+      let firstHalf = arr.slice(0,i);
+      let secondHalf = arr.slice(i, arr.length);
+      totalPerms.push(firstHalf.concat(last, secondHalf))
     }
-  })
+  }
   return totalPerms;
-};
+}
 
 console.log([1,2,3].permutations());
--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -4,14 +4,14 @@ class Array
   def permutations
     return [self] if self.length <= 1
 
-    perms = self[0..-2].permutations
-    last = [self.last]
+    perms = self[1..-1].permutations
+    first = [self.first]
     total_perms = []
-    perms.each do |el|
-      (0..el.length).each do |idx|
-        first_half = el[0...idx]
-        second_half = el[idx..-1]
-        total_perms << first_half + last + second_half
+    perms.each do |arr|
+      (0..arr.length).each do |id|
+        first_half = arr[0...id]
+        second_half = arr[id..-1]
+        total_perms << first_half + first + second_half
       end
     end
     total_perms
