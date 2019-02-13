0de2c566a98d557d99083f1ee9176f6cb0454a8b
Winston Galas <BrianisWinston@yahoo.com>
2019-02-13 01:12:24 -0800

    Solve permutation in js

--- Common_Concepts/permutation.js
+++ Common_Concepts/permutation.js
@@ -1,21 +1,17 @@
-const permutations = (arr) => {
-  if (arr.length <= 1) return [arr];
-  // debugger
-  let totalPerms = [];
-  let first = [arr[0]]; // [1]
-  let perms = permutations(arr.slice(1)); // [[2, 3], [3, 2]]
-  perms.forEach( perm => {
-    for (i = 0;i < perm.length + 1; i++) { // i = 2 , perm = [3, 2]
-      let firstHalf = perm.slice(0, i); // [3, 2]
-      let secondHalf = perm.slice(i) //[]
-      let together = firstHalf.concat(first, secondHalf); // [3, 2, 1]
-      totalPerms.push(together);
-    }
-  });
-  return totalPerms; //[[2, 3], [3, 2], [1, 2, 3], [2, 1, 3], [2, 3, 1]
-  // [1, 3, 2], [3, 1, 2], [3, 2, 1]]
-}
+Array.prototype.permutations = function () {
+  if (this.length <= 1) return [this];
 
-console.log(permutations([1,2,3]) == [
-  [1, 2, 3], [2, 1, 3], [2, 3, 1],
-  [1, 3, 2], [3, 1, 2], [3, 2, 1]]);
+  let perms = this.slice(0, this.length - 1).permutations();
+  let totalPerms = [];
+  let last = this[this.length - 1];
+  perms.forEach( el => {
+    for (let i = 0; i <= el.length; i++) {
+      let firstHalf = el.slice(0, i);
+      let secondHalf = el.slice(i, el.length);
+      totalPerms.push(firstHalf.concat(last, secondHalf));
+    }
+  })
+  return totalPerms;
+};
+
+console.log([1,2,3].permutations());
--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -4,36 +4,18 @@ class Array
   def permutations
     return [self] if self.length <= 1
 
-    perms = self[1..-1].permutations
-    first_num = [self.first]
+    perms = self[0..-2].permutations
+    last = [self.last]
     total_perms = []
     perms.each do |el|
-      (0..el.length).each do |id|
-        first_half = el[0...id]
-        second_half = el[id..-1]
-        total_perms << first_half + first_num + second_half
+      (0..el.length).each do |idx|
+        first_half = el[0...idx]
+        second_half = el[idx..-1]
+        total_perms << first_half + last + second_half
       end
     end
     total_perms
   end
 end
 
-# class Array
-#   def permutations
-#     # debugger
-#     return [self] if self.length <= 1
-#
-#     last_num = [self.last]
-#     total_perms = []
-#     perms = self[0..-2].permutations
-#     perms.each { |el|
-#       (0..el.length).each { |idx|
-#         first_half = el[0...idx]
-#         second_half = el[idx..-1]
-#         total_perms << first_half + last_num + second_half
-#       }
-#     }
-#     total_perms
-#   end
-# end
 p [1, 2, 3].permutations
