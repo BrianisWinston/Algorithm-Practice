6e6df5282010660dacbe1981e964e7c86394a432
Winston Galas <BrianisWinston@yahoo.com>
2019-02-12 23:27:48 -0800

    Start merge sort in ruby

--- Common_Concepts/merge_sort.rb
+++ Common_Concepts/merge_sort.rb
@@ -1,28 +1,4 @@
-class Array
-  def merge_sort(&prc)
-    return self if self.length <= 1
-    prc ||= Proc.new { |x, y| y <=> x }
-    mid = self.length / 2
-    left = self[0...mid].merge_sort(&prc)
-    right = self[mid..-1].merge_sort(&prc)
 
-    return merge(left, right, &prc)
-  end
-
-  def merge(left, right, &prc)
-    answer = []
-    until (left.empty? || right.empty?)
-      if (prc.call(left[0], right[0]) <= 0)
-        answer << right.shift
-      else
-        answer << left.shift
-      end
-    end
-    answer.concat(left)
-    answer.concat(right)
-    answer
-  end
-end
 
 prc1 = Proc.new { |x, y| x <=> y }
 prc2 = Proc.new { |x, y| y <=> x }
@@ -31,3 +7,19 @@ p [100, 101, 59, 70, 0, 98].merge_sort
 puts "[1, 2, 3, 4, 5].merge_sort(&prc1) === [5, 4, 3, 2, 1] ==> #{[1, 2, 3, 4, 5].merge_sort(&prc1) === [5, 4, 3, 2, 1]}"
 
 puts "[5, 4, 3, 2, 1].merge_sort(&prc2) === [1, 2, 3, 4, 5] ==> #{[5, 4, 3, 2, 1].merge_sort(&prc2) === [1, 2, 3, 4, 5]}"
+
+# Mergesort, if implemented to create arrays in the recursive calls,
+# will create many of them, but they won't coexist at the same time.
+# In every recursive call you create an array (or 2 depending on an
+# implementation) for merging and they take no more than O(n) space,
+# and then when the merging is done, these arrays are deleted and some
+# new ones will be created after a moment in some other recursive call.
+# If you counted how much space all the arrays that ever have been created
+# took, it'd be O(n log n), but you don't need to care about this
+# information - you don't need more than O(n) space, because when you
+# need to create an array, all the other ones don't longer exist and
+# don't occupy any memory. Note that you can simply declare 2 - or 3 -
+# arrays in the beginning, each the length of n, and then store the
+# sequence in one of them, while using the other for merging, it will
+# improve the performance as well as show you beyond doubt there's no
+# need for more than O(n) of memory.
