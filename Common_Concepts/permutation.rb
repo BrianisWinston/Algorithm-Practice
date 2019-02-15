44dcd4ef5977adeb24fdcbe9008811a4fd091e2b
Winston Galas <BrianisWinston@yahoo.com>
2019-02-14 18:36:31 -0800

    Solve permutations and subsets for Google interview preparation

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 class Array
   def permutations
     return [self] if self.length <= 1
@@ -7,10 +5,10 @@ class Array
     perms = self[1..-1].permutations
     first = [self.first]
     total_perms = []
-    perms.each do |arr|
-      (0..arr.length).each do |id|
-        first_half = arr[0...id]
-        second_half = arr[id..-1]
+    perms.each do |perm|
+      (0..perm.length).each do |id|
+        first_half = perm[0...id]
+        second_half = perm[id..-1]
         total_perms << first_half + first + second_half
       end
     end
--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,13 +1,22 @@
 const subset = (arr) => {
   if (arr.length === 0) return [[]];
 
-  let first = [arr[arr.length - 1]];
-  let subs = subset(arr.slice(0, arr.length - 1));
-  let newSubs = subs.map( el => el.concat(first));
-
+  let subs = subset(arr.slice(1));
+  let first = [arr[0]];
+  let newSubs = subs.map( el => first.concat(el));
   return subs.concat(newSubs);
 }
 
+
+// const subset = (arr) => {
+//   if (arr.length === 0) return [[]];
+//
+//   let first = [arr[arr.length - 1]];
+//   let subs = subset(arr.slice(0, arr.length - 1));
+//   let newSubs = subs.map( el => el.concat(first));
+//
+//   return subs.concat(newSubs);
+// }
 // elegant solution------------
 // const getAllSubsets = theArray => {
 //   theArray.reduce( (subsets, value) => {
