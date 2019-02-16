4d2b93731d128ff205281e591a82dfd3d42f4e76
Winston Galas <BrianisWinston@yahoo.com>
2019-02-15 21:31:33 -0800

    Redo subsets for practice

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,11 +1,3 @@
-const subset = (arr) => {
-  if (arr.length === 0) return [[]];
-
-  let subs = subset(arr.slice(1));
-  let first = [arr[0]];
-  let newSubs = subs.map( el => first.concat(el));
-  return subs.concat(newSubs);
-}
 
 
 // const subset = (arr) => {
