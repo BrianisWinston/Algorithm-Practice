ef91d7bd152f584acc21a8317d4998debb9dab09
Winston Galas <BrianisWinston@yahoo.com>
2019-02-15 21:47:51 -0800

    Redo subset in rb

--- Common_Concepts/subsets.rb
+++ Common_Concepts/subsets.rb
@@ -1,11 +1,4 @@
 def subsets(arr)
-  return [[]] if arr.empty?
-
-  last_num = arr.last
-  subs = subsets(arr[0...-1])
-  new_subs = subs.map { |el| el + [last_num] }
-
-  subs.concat(new_subs)
 end
 
 p subsets([1, 2, 3, 4])
