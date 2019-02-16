390dcbf34d51ca8fa3f32c7134accb644a64030a
Winston Galas <BrianisWinston@yahoo.com>
2019-02-15 21:46:49 -0800

    Solve subset in js

--- Common_Concepts/subsets.js
+++ Common_Concepts/subsets.js
@@ -1,4 +1,14 @@
+const subset = (arr) => {
+  if (arr.length === 0) return [[]];
 
+  let subs = subset(arr.slice(0, arr.length - 1));
+  let last = arr[arr.length - 1];
+  let newSubs = subs.map( el => el.concat(last));
+
+  return subs.concat(newSubs);
+}
+
+console.log(subset([1, 2, 3, 4]));
 
 // const subset = (arr) => {
 //   if (arr.length === 0) return [[]];
@@ -14,5 +24,3 @@
 //   theArray.reduce( (subsets, value) => {
 //     subsets.concat(subsets.map(set => [value,...set]))
 //   }, [[]] )};
-
-console.log(subset([1, 2, 3, 4]));
