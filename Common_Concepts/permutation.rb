dd6d55fe42595cba77f89b6e23d4e49868f9dfe3
Winston Galas <BrianisWinston@yahoo.com>
2019-02-17 23:34:12 -0800

    Start permutation in rb

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,18 +1,6 @@
 class Array
   def permutations
-    return [self] if self.length == 1
-
-    perms = self[1..-1].permutations
-    first_num = [self.first]
-    total_perms = []
-    perms.each do |perm|
-      (0..perm.length).each do |idx|
-        first_half = perm[0...idx]
-        second_half = perm[idx..-1]
-        total_perms << first_half + first_num + second_half
-      end
-    end
-    total_perms
+    
   end
 end
 
