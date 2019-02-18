e42d3e1c149cfe7fdd7d01e694a2620ea534357d
Winston Galas <BrianisWinston@yahoo.com>
2019-02-17 23:41:21 -0800

    Solve in rb

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,6 +1,19 @@
 class Array
   def permutations
-    
+    return [self] if self.length == 1
+
+    perms = self[0..-2].permutations
+    last_num = [self.last]
+    total_perms = []
+    perms.each do |perm|
+      (0..perm.length).each do |id|
+        first_half = perm[0...id]
+        second_half = perm[id..-1]
+        total_perms << second_half + last_num + first_half
+      end
+    end
+
+    total_perms
   end
 end
 
