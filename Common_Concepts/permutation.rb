41082627a670e0739688f241d1e8ac10774cd9bf
Winston Galas <BrianisWinston@yahoo.com>
2019-02-19 23:45:37 -0800

    Solve perms in rb

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,2 +1,18 @@
+class Array
+  def permutations
+    return [self] if self.length == 1
 
-p [1, 2, 3].permutations
+    perms = self[0..-2].permutations
+    last = [self.last]
+    total_perms = []
+    perms.each do |perm|
+      (0..perm.length).each do |id|
+        first_half = perm[0...id]
+        second_half = perm[id..-1]
+        total_perms << first_half + last + second_half
+      end
+    end
+    total_perms
+  end
+end
+p [1, 2, 3, 4].permutations
