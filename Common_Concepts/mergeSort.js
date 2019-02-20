05faa06983eb299718ed94e329421e23ae53ceb9
Winston Galas <BrianisWinston@yahoo.com>
2019-02-20 00:20:00 -0800

    Start merge sort in js

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -1,37 +1,4 @@
-Array.prototype.mergeSort = function (cb) {
-  if (this.length == 1) return this;
-  if (typeof cb !== 'function') {
-    cb = compA;
-  }
 
-  let mid = Math.floor(this.length / 2);
-  let left = this.slice(0, mid).mergeSort(cb);
-  let right = this.slice(mid).mergeSort(cb);
-
-  return merge(left, right, cb);
-};
-
-const merge = (l, r, cb) => {
-  let answer = [];
-  while (l.length !== 0 && r.length !== 0) {
-    if (cb(l[0], r[0]) <= 0) {
-      answer.push(r.shift());
-    } else {
-      answer.push(l.shift());
-    }
-  };
-  return answer.concat(l, r);
-}
-
-const compA = (a, b) => {
-  if (a < b) {
-    return -1;
-  } else if (a === b) {
-    return 0;
-  } else {
-    return 1;
-  };
-};
 
 const compB = (a, b) => {
   if (a > b) {
