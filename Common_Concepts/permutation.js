faa98aa560590a1c1ad615a54e963278bc9db2ff
Winston Galas <BrianisWinston@yahoo.com>
2019-02-19 23:38:25 -0800

    Solve perms in js

--- Common_Concepts/permutation.js
+++ Common_Concepts/permutation.js
@@ -1,3 +1,18 @@
+Array.prototype.permutations = function () {
+  if (this.length == 1) return [this];
 
+  let perms = this.slice(1).permutations();
+  let first = [this[0]];
+  let totalPerms = [];
+  for (let perm of perms) {
+    for (let i = 0; i <= perm.length; i++) {
+      let firstHalf = perm.slice(0, i);
+      let secondHalf = perm.slice(i);
+      totalPerms.push(firstHalf.concat(first, secondHalf));
+    }
+  }
+
+  return totalPerms;
+};
 
 console.log([1,2,3].permutations());
