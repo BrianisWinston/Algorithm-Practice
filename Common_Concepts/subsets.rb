4dda00210da077234d236884b42f5835f128be70
Winston Galas <BrianisWinston@yahoo.com>
2019-02-19 23:28:04 -0800

    Start subset in rb

--- Common_Concepts/subsets.rb
+++ Common_Concepts/subsets.rb
@@ -1,9 +1,2 @@
-def subsets(arr)
-  return [[]] if arr.empty?
 
-  subs = subsets(arr[0...-1])
-  last = [arr.last]
-  new_subs = subs.map { |el| el + last }
-  subs.concat(new_subs)
-end
 p subsets([1, 2, 3, 4])
