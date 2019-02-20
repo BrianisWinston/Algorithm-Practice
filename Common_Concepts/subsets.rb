cea3a282aea5fee27dc6be654c763b142c120889
Winston Galas <BrianisWinston@yahoo.com>
2019-02-19 23:30:01 -0800

    Solve subset in rb

--- Common_Concepts/subsets.rb
+++ Common_Concepts/subsets.rb
@@ -1,2 +1,10 @@
+def subsets(arr)
+  return [[]] if arr.length == 0
 
+  subs = subsets(arr[0..-2])
+  last = [arr.last]
+  new_subs = subs.map { |sub| sub + last }
+
+  new_subs.concat(subs)
+end
 p subsets([1, 2, 3, 4])
