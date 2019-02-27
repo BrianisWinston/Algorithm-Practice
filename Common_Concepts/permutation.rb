a0d6fb91b688575858186fbf603d99f9ced50725
Winston Galas <BrianisWinston@yahoo.com>
2019-02-27 02:37:50 -0800

    Redo permutation in rb

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,20 +1,2 @@
-require 'byebug'
 
-class Array
-  def permutations
-    return [self] if self.length == 1
-    # debugger
-    perms = self[0..-2].permutations
-    last = [self.last]
-    total_perms = []
-    perms.each do |perm|
-      (0..perm.length).each do |id|
-        first_half = perm[0...id]
-        second_half = perm[id..-1]
-        total_perms << first_half + last + second_half
-      end
-    end
-    total_perms
-  end
-end
 p [1, 2, 3, 4].permutations
