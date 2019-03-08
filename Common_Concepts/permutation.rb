621ea5a7e0eed5c167c07f0320c7b549333061e8
Winston Galas <BrianisWinston@yahoo.com>
2019-03-08 00:04:36 -0800

    Solve permutations in rb

--- Common_Concepts/permutation.rb
+++ Common_Concepts/permutation.rb
@@ -1,5 +1,23 @@
 require 'byebug'
 
+class Array
+  def permutations
+    return [self] if self.length == 1
+
+    perms = self[1..-1].permutations
+    first_num = self.first
+    total_perms = []
+
+    perms.each do |perm|
+      (0..perm.length).each do |id|
+        first_half = perm[0...id]
+        second_half = perm[id..-1]
+        total_perms << first_half + [first_num] + second_half
+      end
+    end
+    total_perms
+  end
+end
 
 # class String
 #   def permutations
