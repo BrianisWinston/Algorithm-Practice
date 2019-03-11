ab38da0929977462a9d6dcc82eeda54d948e7bec
Winston Galas <BrianisWinston@yahoo.com>
2019-03-11 00:09:36 -0700

    Solve fibs in rb recursively and iteratively

--- Common_Concepts/fib_sums.rb
+++ Common_Concepts/fib_sums.rb
@@ -1,34 +1,46 @@
-# if num == 0
-#   return 0
-# elsif num == 1
-#   return 1
+# def fib(num)
+#   return 0 if num == 0
+#   return 1 if num == 1
+#
+#   fib(num - 1) + fib(num - 2)
+# end
+
+def fib(num)
+  base = [0, 1]
+  return base[num] if num <= 1
+
+  count = 1
+  while count < num
+    base << base[-1] + base[-2]
+    count += 1
+  end
+  base.last
+end
+
+# FIBS = {
+#   0 => 0,
+#   1 => 1
+# }
+# def fib_seq(num)
+#   return FIBS[num] if num <= 1
+#   if FIBS[num] != nil
+#     return FIBS[num]
+#   else
+#     FIBS[num] = fib_seq(num - 1) + fib_seq(num - 2)
+#     return FIBS[num]
+#   end
 # end
 #
-# fib_seq(num - 1) + fib_seq(num - 2)
-FIBS = {
-  0 => 0,
-  1 => 1
-}
-def fib_seq(num)
-  return FIBS[num] if num <= 1
-  if FIBS[num] != nil
-    return FIBS[num]
-  else
-    FIBS[num] = fib_seq(num - 1) + fib_seq(num - 2)
-    return FIBS[num]
-  end
-end
-
-p fib_seq(7)
-p FIBS
-p fib_seq(0)
-p fib_seq(1)
-@cache = [0,1]
-
-def fib(n)
-  p self
-  return @cache[n] if @cache[n]
-  @cache[n] = fib(n-1) + fib(n-2)
-end
-p "outside of function #{self}"
-p fib(10)
+# p fib_seq(7)
+# p FIBS
+# p fib_seq(0)
+# p fib_seq(1)
+# @cache = [0,1]
+#
+# def fib(n)
+#   p self
+#   return @cache[n] if @cache[n]
+#   @cache[n] = fib(n-1) + fib(n-2)
+# end
+# p "outside of function #{self}"
+p fib(10) == 55
