bd8f57f16bc07a54314a20ef8352aade0890885c
Winston Galas <BrianisWinston@yahoo.com>
2019-03-13 22:41:35 -0700

    Solve bsearch in rb

--- Common_Concepts/binary_search.rb
+++ Common_Concepts/binary_search.rb
@@ -1,4 +1,18 @@
+class Array
+  def my_bsearch(num)
+    return nil if self.length == 0
 
+    mid = self.length / 2
+    if self[mid] == num
+      return mid
+    elsif self[mid] > num
+      return self[0...mid].my_bsearch(num)
+    else
+      new_id = self[mid + 1..-1].my_bsearch(num)
+      return new_id == nil ? nil : mid + new_id + 1
+    end
+  end
+end
 
 p [1, 3, 4, 5, 10, 11].my_bsearch(1)
 p [1, 3, 4, 5, 10, 11].my_bsearch(3)
