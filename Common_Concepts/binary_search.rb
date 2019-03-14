e0c860717f94ea89ead326bedee2ebaf3c48254d
Winston Galas <BrianisWinston@yahoo.com>
2019-03-13 22:38:07 -0700

    Redo bsearch in rb for practice

--- Common_Concepts/binary_search.rb
+++ Common_Concepts/binary_search.rb
@@ -1,18 +1,4 @@
-class Array
-  def my_bsearch(num)
-    return nil if self.length == 0
 
-    mid = self.length / 2
-    if self[mid] == num
-      mid
-    elsif self[mid] > num
-      self[0...mid].my_bsearch(num)
-    else
-      recurse = self[mid + 1..-1].my_bsearch(num)
-      recurse == nil ? nil : recurse + mid + 1
-    end
-  end
-end
 
 p [1, 3, 4, 5, 10, 11].my_bsearch(1)
 p [1, 3, 4, 5, 10, 11].my_bsearch(3)
