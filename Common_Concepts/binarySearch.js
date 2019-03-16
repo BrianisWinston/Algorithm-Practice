257f8768d5454b87df0cd6b0b2adb1563766a5bb
Winston Galas <BrianisWinston@yahoo.com>
2019-03-16 15:55:01 -0700

    Solve bsearch in js

--- Common_Concepts/binarySearch.js
+++ Common_Concepts/binarySearch.js
@@ -1,4 +1,16 @@
+const bSearch = (arr, num) => {
+  if (arr.length == 0) return null;
 
+  let mid = Math.floor(arr.length / 2);
+  if (arr[mid] == num) {
+    return mid;
+  } else if (arr[mid] > num) {
+    return bSearch(arr.slice(0, mid), num);
+  } else {
+    let newId = bSearch(arr.slice(mid + 1), num);
+    return (newId === null ? null : mid + 1 + newId);
+  }
+};
 
 let arr1 = [1, 4, 9, 10, 19, 21];
 console.log(bSearch(arr1, 1));
