f22d9c553190d1e9feb19748b047ab888f05d4c3
Winston Galas <BrianisWinston@yahoo.com>
2019-03-16 15:49:09 -0700

    Redo bsearch in js

--- Common_Concepts/binarySearch.js
+++ Common_Concepts/binarySearch.js
@@ -1,17 +1,4 @@
-const bSearch = function(arr, num) {
-  if (arr.length == 0) return null;
 
-  let mid = Math.floor(arr.length / 2);
-
-  if (arr[mid] == num) {
-    return mid;
-  } else if (arr[mid] > num) {
-    return bSearch(arr.slice(0, mid), num);
-  } else {
-    let recId = bSearch(arr.slice(mid + 1), num);
-    return recId === null ? null : mid + recId + 1;
-  };
-}
 
 let arr1 = [1, 4, 9, 10, 19, 21];
 console.log(bSearch(arr1, 1));
