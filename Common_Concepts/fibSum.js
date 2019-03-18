7eb0a68e1e2caed31c79083bc3e21b2d44001ee4
Winston Galas <BrianisWinston@yahoo.com>
2019-03-17 23:00:47 -0700

    Solve dibs with dynamic programming in js

---
+++ Common_Concepts/fibSum.js
@@ -0,0 +1,30 @@
+// DYNAMIC PROGRAMMING-----------------------------------
+
+FIBSUMS = {
+  0: 0,
+  1: 1
+};
+
+const fibSums = (num) => {
+  if (FIBSUMS[num] !== undefined) {
+    return FIBSUMS[num];
+  } else {
+    FIBSUMS[num] = fibSums(num - 1) + fibSums(num - 2);
+  }
+  return fibSums(num - 1) + fibSums(num - 2);
+  return fibSums[num];
+};
+
+
+console.log(fibSums(0) == 0)
+console.log(fibSums(1) == 1)
+console.log(fibSums(2) == 1)
+console.log(fibSums(3) == 2)
+console.log(fibSums(4) == 3)
+console.log(fibSums(5) == 5)
+console.log(fibSums(6) == 8)
+console.log(fibSums(7) == 13)
+console.log(fibSums(8) == 21)
+console.log(fibSums(9) == 34)
+console.log(fibSums(10) == 55)
+console.log(fibSums(45))
--- Common_Concepts/fib_sums.rb
+++ Common_Concepts/fib_sums.rb
@@ -43,4 +43,15 @@ end
 #   @cache[n] = fib(n-1) + fib(n-2)
 # end
 # p "outside of function #{self}"
+
+p fib(0)
+p fib(1)
+p fib(2)
+p fib(3)
+p fib(4)
+p fib(5)
+p fib(6)
+p fib(7)
+p fib(8)
+p fib(9)
 p fib(10) == 55
