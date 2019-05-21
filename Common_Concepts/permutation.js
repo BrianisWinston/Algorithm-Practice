a015962bf583346c36c5f441f6f1453d84095561
Winston Galas <BrianisWinston@yahoo.com>
2019-05-21 00:42:34 -0700

    Solve permutations in js

--- Common_Concepts/permutation.js
+++ Common_Concepts/permutation.js
@@ -1,3 +1,34 @@
+Array.prototype.permutations = function() {
+  if (this.length == 1) return [this];
 
+  let perms = this.slice(0, this.length - 1).permutations();
+  let last = [this[this.length - 1]];
+  let totalPerms = [];
+  for (let perm of perms) {
+    for (let i = 0; i <= perm.length; i++) {
+      firstHalf = perm.slice(0, i);
+      secondHalf = perm.slice(i);
+      totalPerms.push(firstHalf.concat(last, secondHalf));
+    };
+  };
+  return totalPerms;
+};
+
+// Array.prototype.permutations = function () {
+//   if (this.length == 1) return [this];
+//
+//   let perms = this.slice(1).permutations();
+//   let first = [this[0]];
+//   let totalPerms = [];
+//   for (let perm of perms) {
+//     for (let i = 0; i <= perm.length; i++) {
+//       let firstHalf = perm.slice(0, i);
+//       let secondHalf = perm.slice(i);
+//       totalPerms.push(firstHalf.concat(first, secondHalf));
+//     }
+//   }
+//
+//   return totalPerms;
+// };
 
 console.log([1,2,3].permutations());
