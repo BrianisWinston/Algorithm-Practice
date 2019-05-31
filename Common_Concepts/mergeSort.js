646576606985d5c93786e727998d7003fee2b378
Winston Galas <BrianisWinston@yahoo.com>
2019-05-31 15:09:24 -0700

    Solve mergesort in js

--- Common_Concepts/mergeSort.js
+++ Common_Concepts/mergeSort.js
@@ -42,7 +42,7 @@
 // };
 
 Array.prototype.mergeSort = function (cb) {
-  if (this.length === 1) return this;
+  if (this.length <= 1) return this;
   if (typeof cb != 'function') cb = compA;
 
   let mid = Math.floor(this.length / 2);
@@ -54,32 +54,30 @@ Array.prototype.mergeSort = function (cb) {
 
 const merge = (l, r, cb) => {
   let answer = [];
-
   while (l.length != 0 && r.length != 0) {
-    if (cb(l[0], r[0]) == -1) {
-      answer.push(l.shift());
-    } else {
+    if (cb(l[0], r[0]) <= 0) {
       answer.push(r.shift());
+    } else {
+      answer.push(l.shift());
     }
   }
-
   return answer.concat(l, r);
-}
+};
 
 const compA = (a, b) => {
-  if (a > b) {
-    return -1;
-  } else if (a === b) {
+  if (a < b) {
+    return 1;
+  } else if (a == b) {
     return 0;
   } else {
-    return 1;
+    return -1;
   }
 };
 
 const compB = (a, b) => {
   if (a > b) {
     return 1;
-  } else if (a === b) {
+  } else if (a == b) {
     return 0;
   } else {
     return -1;
