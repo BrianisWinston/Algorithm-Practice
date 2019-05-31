5427b8e26b4bd5d799660427651c575af896a7d7
Winston Galas <BrianisWinston@yahoo.com>
2019-05-31 15:35:02 -0700

    Solve fib sums with dynamic programming in ruby

--- Common_Concepts/fib_sums.rb
+++ Common_Concepts/fib_sums.rb
@@ -5,16 +5,29 @@
 #   fib(num - 1) + fib(num - 2)
 # end
 
-def fib(num)
-  base = [0, 1]
-  return base[num] if num <= 1
+# def fib(num)
+#   base = [0, 1]
+#   return base[num] if num <= 1
+#
+#   count = 1
+#   while count < num
+#     base << base[-1] + base[-2]
+#     count += 1
+#   end
+#   base.last
+# end
 
-  count = 1
-  while count < num
-    base << base[-1] + base[-2]
-    count += 1
+SOLUTIONS = {
+  0 => 0,
+  1 => 1
+}
+
+def fib(num)
+  if SOLUTIONS[num] != nil
+    SOLUTIONS[num]
+  else
+    SOLUTIONS[num] = fib(num - 1) + fib(num - 2)
   end
-  base.last
 end
 
 # FIBS = {
@@ -44,14 +57,15 @@ end
 # end
 # p "outside of function #{self}"
 
-p fib(0)
-p fib(1)
-p fib(2)
-p fib(3)
-p fib(4)
-p fib(5)
-p fib(6)
-p fib(7)
-p fib(8)
-p fib(9)
+p fib(0) == 0
+p fib(1) == 1
+p fib(2) == 1
+p fib(3) == 2
+p fib(4) == 3
+p fib(5) == 5
+p fib(6) == 8
+p fib(7) == 13
+p fib(8) == 21
+p fib(9) == 34
 p fib(10) == 55
+p fib(45)
