dfc6d4847de1f95719eaaf091e6353b9eaf428ea
Winston Galas <BrianisWinston@yahoo.com>
2019-06-02 23:18:58 -0700

    Solve bubble sort with cbs in js

--- Common_Concepts/bubbleSort.js
+++ Common_Concepts/bubbleSort.js
@@ -1,7 +1,43 @@
+Array.prototype.bubbleSort = function (cb) {
+  if (typeof cb != 'function') cb = compA;
+  let sorted = false;
+  while (sorted == false) {
+    sorted = true;
+    for (let i = 0; i < this.length - 1; i++) {
+      if (cb(this[i], this[i + 1]) == -1) {
+        [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        sorted = false;
+      };
+    };
+    sorted;
+  };
+  return this;
+};
 
+const compA = (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a == b) {
+    return 0;
+  } else {
+    return -1;
+  };
+};
+
+const compB = (a, b) => {
+ if (a > b) {
+   return -1;
+ } else if (a == b) {
+   return 0;
+ } else {
+   return 1;
+ };
+};
 
 // console.log(pojo instanceof Object);
-console.log([1,6,3,8,6,5,10].bubbleSort()); // [1,3,5,6,6,8.10]
+console.log([1,6,3,8,6,5,10].bubbleSort()); // [1,3,5,6,6,8,10]
+console.log([1,6,3,8,6,5,10].bubbleSort(compA)); // [10,8,6,6,5,3,1]
+console.log([1,6,3,8,6,5,10].bubbleSort(compB)); // [1,3,5,6,6,8,10]
 
 // Array.prototype.bubbleSort = () => {
 //   console.log(this);
