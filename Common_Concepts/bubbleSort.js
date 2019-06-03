38ef8f48230d14fd0a54bb6225589c366d89c3fd
Winston Galas <BrianisWinston@yahoo.com>
2019-06-02 22:37:55 -0700

    Redo bubble sort in js

--- Common_Concepts/bubbleSort.js
+++ Common_Concepts/bubbleSort.js
@@ -1,17 +1,4 @@
-Array.prototype.bubbleSort = function() {
-  // console.log(`yeeee ${this.what}`);
-  let sorted = false;
-  while (sorted !== true) {
-    sorted = true;
-    for (let i = 0; i < this.length - 2; i++) {
-      if (this[i] > this[i + 1]) {
-        [this[i], this[i + 1]] = [this[i + 1], this[i]];
-        sorted = false;
-      }
-    }
-    sorted
-  }
-};
+
 
 // console.log(pojo instanceof Object);
 console.log([1,6,3,8,6,5,10].bubbleSort()); // [1,3,5,6,6,8.10]
